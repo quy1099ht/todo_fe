09012108b536287d47ee305891d8db34ee44ac95
Quy Tran <quy.tran@bstarsolutions.com>
2022-06-28 11:57:46 +0700

    add dynamic rendering

---
+++ src/app/features/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ["../login-feature/login-feature.component.scss"]
+})
+export class RegisterComponent implements OnInit {
+  validateForm!: FormGroup;
+  passwordVisible = false;
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+    } else {
+      
+    }
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      remember: [true]
+    });
+  }       
+
+}
